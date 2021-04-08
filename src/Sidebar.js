e56670bea64991153f681806869c2dbb70cda2ab
Dawid Spisak <dawidspisak@gmail.com>
2021-04-08 19:03:06 +0200

    first commit

---
+++ src/Sidebar.js
@@ -0,0 +1,16 @@
+import './Sidebar.css'
+import TwiiterIcon from '@material-ui/icons/Twitter'
+import SidebarOption from './SidebarOption'
+const Sidebar = () => {
+    return (
+        <div>
+            sidebar
+            <SidebarOption />
+            <TwiiterIcon>
+
+            </TwiiterIcon>
+        </div>
+    );
+}
+
+export default Sidebar;
---
+++ src/SidebarOption.js
@@ -0,0 +1,10 @@
+import './Sidebar.css'
+const SidebarOption = () => {
+    return (
+        <div>
+            option
+        </div>
+    );
+}
+
+export default SidebarOption;
